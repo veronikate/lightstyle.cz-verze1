4208477788f79caea3c1542036ce0dee62aebb87
Veronika Teschen <veronika.zittova@protonmail.com>
2022-05-04 09:59:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { $ } = require("dom7");
+
 $("#kuchyne_link").click(function () {
 	$("#arrow_kuchyne").toggleClass("uparrow");
 	$("#kuchyne_menu_drawer").slideToggle();
@@ -13,7 +15,6 @@ $("#kuchyne_link").click(function () {
 	$("#arrow_akce").removeClass("uparrow");
 	$("#arrow_kontakt").removeClass("uparrow");
 	$("#arrow_realizace").removeClass("uparrow");
-	$(".themodal").css("display", "block");
 });
 /*$("#skrine_link").click(function () {
 	$("#arrow_skrine").toggleClass("uparrow");
@@ -42,7 +43,6 @@ $("#showroomy_link").click(function () {
 	$("#arrow_akce").removeClass("uparrow");
 	$("#arrow_kontakt").removeClass("uparrow");
 	$("#arrow_realizace").removeClass("uparrow");
-	$(".themodal").css("display", "block");
 });
 $("#spotrebice_link").click(function () {
 	$("#arrow_spotrebice").toggleClass("uparrow");
@@ -59,7 +59,6 @@ $("#spotrebice_link").click(function () {
 	$("#arrow_akce").removeClass("uparrow");
 	$("#arrow_kontakt").removeClass("uparrow");
 	$("#arrow_realizace").removeClass("uparrow");
-	$(".themodal").css("display", "block");
 });
 $("#akce_link").click(function () {
 	$("#arrow_akce").toggleClass("uparrow");
@@ -76,7 +75,6 @@ $("#akce_link").click(function () {
 	$("#arrow_kuchyne").removeClass("uparrow");
 	$("#arrow_kontakt").removeClass("uparrow");
 	$("#arrow_realizace").removeClass("uparrow");
-	$(".themodal").css("display", "block");
 });
 $("#kontakt_link").click(function () {
 	$("#arrow_kontakt").toggleClass("uparrow");
@@ -92,7 +90,6 @@ $("#kontakt_link").click(function () {
 	$("#arrow_akce").removeClass("uparrow");
 	$("#arrow_kuchyne").removeClass("uparrow");
 	$("#arrow_realizace").removeClass("uparrow");
-	$(".themodal").css("display", "block");
 });
 
 $("#realizace_link").click(function () {
@@ -110,7 +107,6 @@ $("#realizace_link").click(function () {
 	$("#arrow_akce").removeClass("uparrow");
 	$("#arrow_kontakt").removeClass("uparrow");
 	$("#arrow_kuchyne").removeClass("uparrow");
-	$(".themodal").css("display", "block");
 });
 
 $(".openbtn").click(function () {
@@ -125,58 +121,115 @@ var viewportWidth = $(window).width();
 
 $(".header_button").click(function () {
 	console.log(viewportWidth);
+	$(".modal").toggleClass("modalon");
 });
+<script src="jssor.slider-28.1.0.min.js" type="text/javascript"></script>;
 
-if (viewportWidth <= 600) {
-	$(".footer").css("display", "none");
-	$(".footer_phone").css("display", "block");
+window.jssor_1_slider_init = function () {
+	var jssor_1_options = {
+		$AutoPlay: 1,
+		$AutoPlaySteps: 5,
+		$SlideDuration: 160,
+		$SlideWidth: 200,
+		$SlideSpacing: 3,
+		$ArrowNavigatorOptions: {
+			$Class: $JssorArrowNavigator$,
+			$Steps: 5,
+		},
+		$BulletNavigatorOptions: {
+			$Class: $JssorBulletNavigator$,
+			$SpacingX: 16,
+			$SpacingY: 16,
+		},
+	};
+
+	var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+	/*#region responsive code begin*/
+
+	var MAX_WIDTH = 980;
+
+	function ScaleSlider() {
+		var containerElement = jssor_1_slider.$Elmt.parentNode;
+		var containerWidth = containerElement.clientWidth;
+
+		if (containerWidth) {
+			var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+			jssor_1_slider.$ScaleWidth(expectedWidth);
+		} else {
+			window.setTimeout(ScaleSlider, 30);
+		}
+	}
+
+	ScaleSlider();
+
+	$Jssor$.$AddEvent(window, "load", ScaleSlider);
+	$Jssor$.$AddEvent(window, "resize", ScaleSlider);
+	$Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+	/*#endregion responsive code end*/
+};
+
+if (document.getElementById("sliderb_container")) {
+	jQuery(document).ready(function ($) {
+		var _SlideshowTransitions = [
+			{
+				$Duration: 1200,
+				x: -0.3,
+				$During: { $Left: [0.3, 0.7] },
+				$Easing: {
+					$Left: $JssorEasing$.$EaseInCubic,
+					$Opacity: $JssorEasing$.$EaseLinear,
+				},
+				$Opacity: 2,
+			},
+			{
+				$Duration: 1200,
+				x: 0.3,
+				$SlideOut: true,
+				$Easing: {
+					$Left: $JssorEasing$.$EaseInCubic,
+					$Opacity: $JssorEasing$.$EaseLinear,
+				},
+				$Opacity: 2,
+			},
+		];
+		var options = {
+			$AutoPlay: true,
+			$AutoPlaySteps: 1,
+			$AutoPlayInterval: 4000,
+			$PauseOnHover: 1,
+			$ArrowKeyNavigation: false,
+			$SlideDuration: 500,
+			$MinDragOffsetToSlide: 20,
+			$SlideSpacing: 0,
+			$DisplayPieces: 1,
+			$ParkingPosition: 0,
+			$UISearchMode: 1,
+			$PlayOrientation: 1,
+			$DragOrientation: 3,
+
+			$SlideshowOptions: {
+				$Class: $JssorSlideshowRunner$,
+				$Transitions: _SlideshowTransitions,
+				$TransitionsOrder: 1,
+				$ShowLink: true,
+			},
+			$ArrowNavigatorOptions: {
+				$Class: $JssorArrowNavigator$,
+				$ChanceToShow: 2,
+				$Steps: 1,
+			},
+		};
+		var jssor_sliderb = new $JssorSlider$("sliderb_container", options);
+		function ScaleSlider() {
+			var parentWidth = jssor_sliderb.$Elmt.parentNode.clientWidth;
+			if (parentWidth) jssor_sliderb.$ScaleWidth(Math.min(parentWidth, 1098));
+			else window.setTimeout(ScaleSlider, 30);
+		}
+		ScaleSlider();
+		if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+			$(window).bind("resize", ScaleSlider);
+		}
+	});
 }
-
-if (viewportWidth >= 600) {
-	$(".footer").css("display", "flex");
-	$(".footer_phone").css("display", "none");
-}
-if (viewportWidth <= 600) {
-	$("#cover_title").html(
-		"Zákazník a kvalita naší práce<br> je pro nás na prvním místě"
-	);
-}
-
-var navbar_width = $(".navbar-nav").css("width");
-var navbar_width_int = parseFloat(navbar_width);
-
-var count = (viewportWidth - navbar_width_int) / 2;
-console.log(viewportWidth);
-console.log(navbar_width);
-console.log(count);
-
-$(".navbar-nav").css("left", count + "px");
-
-$("#footer_button_one").click(function () {
-	$("#footer_content_one").toggleClass("content_show");
-});
-
-$("#footer_button_two").click(function () {
-	$("#footer_content_two").toggleClass("content_show");
-});
-$("#footer_button_three").click(function () {
-	$("#footer_content_three").toggleClass("content_show");
-});
-
-$(".themodal").click(function () {
-	$(".themodal").css("display", "none");
-
-	$("#realizace_menu_drawer").slideUp();
-	$("#akce_menu_drawer").slideUp();
-	$("#kuchyne_menu_drawer").slideUp();
-	$("#showroomy_menu_drawer").slideUp();
-	$("#spotrebice_menu_drawer").slideUp();
-	$("#skrine_menu_drawer").slideUp();
-	$("#kontakt_menu_drawer").slideUp();
-	$("#arrow_realizace").removeClass("uparrow");
-	$("#arrow_showroomy").removeClass("uparrow");
-	$("#arrow_spotrebice").removeClass("uparrow");
-	$("#arrow_akce").removeClass("uparrow");
-	$("#arrow_kontakt").removeClass("uparrow");
-	$("#arrow_kuchyne").removeClass("uparrow");
-});
